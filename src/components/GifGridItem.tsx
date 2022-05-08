7721da6b202bb1b0c9e283ef78d6a30f3b745aeb
Kadievka Salcedo <kadieka.s.ks@gmail.com>
2022-05-08 22:03:07 +0200

    Pruebas del componente - GifGridItem

--- src/components/GifGridItem.tsx
+++ src/components/GifGridItem.tsx
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 
 const GifGridItem = ({title, url}: ImageInterface) => {
   return (
-    <div className="card animate__animated animate__fadeIn" >
-      <img src={url} alt={title} />
-      <p>{title}</p>
+    <div data-testid="div-element" className="card animate__animated animate__fadeIn" >
+      <img data-testid="img-element" src={url} alt={title} />
+      <p data-testid="title-element">{title}</p>
     </div>
   )
 }
--- src/tests/components/GifGridItem.test.tsx
+++ src/tests/components/GifGridItem.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import GifGridItem from "../../components/GifGridItem";
 import { ImageInterface } from "../../interfaces/ImageInterface";
 
@@ -15,7 +15,15 @@ describe('GifGridItem', ()=>{
 
     const wrapper = render(<GifGridItem {...input}  />);
 
+    const divElement = screen.getByTestId('div-element');
+    const imgElement = screen.getByTestId('img-element');
+    const titleElement = screen.getByTestId('title-element');
+
     expect(wrapper.asFragment()).toMatchSnapshot();
+
+    expect(divElement).toHaveClass('animate__fadeIn');
+    expect(imgElement).toHaveAttribute('src', 'mock-url');
+    expect(titleElement.innerHTML.trim()).toBe('mock-title');
 
   });
 
